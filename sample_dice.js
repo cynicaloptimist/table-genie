2fe6d2de8db40e5d4cc8186f6f969d1289c48ec7
Evan Bailey <baileyevang@gmail.com>
2017-03-31 20:26:34 -0700

    Add tests

---
+++ sample_dice.js
@@ -0,0 +1,55 @@
+module.exports = {
+  "session": {
+    "new": false,
+    "sessionId": "amzn1.echo-api.session.[unique-value-here]",
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account.[unique-value-here]"
+    },
+    "application": {
+      "applicationId": "amzn1.ask.skill.[unique-value-here]"
+    }
+  },
+  "version": "1.0",
+  "request": {
+    "locale": "en-US",
+    "timestamp": "2016-10-27T21:06:28Z",
+    "type": "IntentRequest",
+    "requestId": "amzn1.echo-api.request.[unique-value-here]",
+    "intent": {
+      "slots": {
+        "HowMany": {
+          "name": "HowMany",
+          "value": "1"
+        },
+        "DieSize": {
+          "name": "DieSize",
+          "value": "6"
+        },
+        "Modifier": {
+          "name": "Modifier",
+          "value": "2"
+        }
+      },
+      "name": "RollDiceIntent"
+    }
+  },
+  "context": {
+    "AudioPlayer": {
+      "playerActivity": "IDLE"
+    },
+    "System": {
+      "device": {
+        "supportedInterfaces": {
+          "AudioPlayer": {}
+        }
+      },
+      "application": {
+        "applicationId": "amzn1.ask.skill.[unique-value-here]"
+      },
+      "user": {
+        "userId": "amzn1.ask.account.[unique-value-here]"
+      }
+    }
+  }
+}
