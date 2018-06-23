75fa38857de3334e6b0645f80976e35260953198
Evan Bailey <baileyevang@gmail.com>
2018-06-22 22:58:31 -0700

    port RollDiceIntent handling to ask-sdk

--- index.ts
+++ index.ts
@@ -1,22 +1,20 @@
 'use strict';
-import * as Alexa from "alexa-sdk";
-import { shuffle, random } from "lodash";
+import * as Alexa from "ask-sdk";
+import { IntentRequest } from "ask-sdk-model";
+import * as _ from "lodash";
 
 const APP_ID = "amzn1.ask.skill.f35f4e73-39b6-4631-af07-824fecad3215";
 
 const resources = require("./resources");
 import { GetRandomEntryFromRedditTable } from "./reddit";
 
-exports.handler = function (event: Alexa.RequestBody<Alexa.Request>, context: Alexa.Context, callback: () => void) {
-    const alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
-    alexa.resources = resources;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-}
-
-const slotOrDefault = (context: any, slotName: string, defaultValue: string): string => {
-    const slot = context.event.request.intent.slots[slotName];
+const slotOrDefault = (input: Alexa.HandlerInput, slotName: string, defaultValue: string): string => {
+    const request = input.requestEnvelope.request as IntentRequest;
+    const slots = request.intent.slots;
+    if (slots === undefined) {
+        return defaultValue;
+    }
+    const slot = slots.slotName;
     if (slot && slot.value && slot.value !== "?") {
         return slot.value;
     }
@@ -26,10 +24,41 @@ const slotOrDefault = (context: any, slotName: string, defaultValue: string): st
 const rollDice = (howMany: number, dieSize: number, modifier: number) => {
     let sum = 0;
     for (let i = 0; i < howMany; i++) {
-        sum += random(1, dieSize);
+        sum += _.random(1, dieSize);
     }
     return sum + modifier;
 }
+
+const RollDiceIntentHandler: Alexa.RequestHandler = {
+    canHandle: (handlerInput) => {
+        const requestType = handlerInput.requestEnvelope.request as IntentRequest;
+        return _.includes(["LaunchRequest", "RollDiceIntent"], requestType.intent.name);
+    },
+    handle: (handlerInput) => {
+        const howMany = parseInt(slotOrDefault(handlerInput, "HowMany", "1"));
+        const dieSize = parseInt(slotOrDefault(handlerInput, "DieSize", "6"));
+        const modifier = parseInt(slotOrDefault(handlerInput, "Modifier", "0"));
+
+        const total = rollDice(howMany, dieSize, modifier);
+
+        let output = "";
+        if (modifier) {
+            output = `I rolled ${howMany} d ${dieSize} plus ${modifier} for a total of ${total}`;
+        } else {
+            output = `I rolled ${howMany} d ${dieSize} for a total of ${total}`;
+        }
+
+        return handlerInput.responseBuilder
+            .speak(output)
+            .withSimpleCard("Rolled Dice", output)
+            .getResponse();
+    }
+}
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        RollDiceIntentHandler)
+    .lambda();
 
 const handlers: any = {
     'LaunchRequest': function () {
@@ -63,7 +92,7 @@ const handlers: any = {
             race = "HUMAN";
         }
 
-        const names = shuffle(namesByRace[race]);
+        const names = _.shuffle(namesByRace[race]);
 
         const name = names[0];
 
