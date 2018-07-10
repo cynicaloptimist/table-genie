a357a9d87825a99e221dbd9f56b1ac8ee0c811fd
Evan Bailey <baileyevang@gmail.com>
2018-07-09 21:54:15 -0700

    Call done param in test

--- reddit.test.ts
+++ reddit.test.ts
@@ -1,13 +1,14 @@
 import { GetRandomEntryFromRedditTable } from "./reddit";
 
 describe("reddit integration", () => {
-    test("", () => {
+    test("", done => {
         expect.assertions(1);
 
         GetRandomEntryFromRedditTable("trinket")
             .then(r => {
                 console.log(r);
                 expect(r).not.toBeUndefined();
+                done();
             });
     });
 });
