59033f3cdba4216d3e4b73db5f9891393091df9a
Evan Bailey <baileyevang@gmail.com>
2018-07-10 00:41:07 -0700

    add test for small payload

---
+++ sample_scotch.js
@@ -0,0 +1,47 @@
+module.exports = {
+  "session": {
+    "new": false,
+    "sessionId": "amzn1.echo-api.session.[unique-value-here]",
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account.[unique-value-here]"
+    },
+    "application": {
+      "applicationId": "amzn1.ask.skill.f35f4e73-39b6-4631-af07-824fecad3215"
+    }
+  },
+  "version": "1.0",
+  "request": {
+    "locale": "en-US",
+    "timestamp": "2016-10-27T21:06:28Z",
+    "type": "IntentRequest",
+    "requestId": "amzn1.echo-api.request.[unique-value-here]",
+    "intent": {
+      "slots": {
+        "SearchTerm": {
+          "name": "SearchTerm",
+          "value": "scotch"
+        }
+      },
+      "name": "SearchForTableIntent"
+    }
+  },
+  "context": {
+    "AudioPlayer": {
+      "playerActivity": "IDLE"
+    },
+    "System": {
+      "device": {
+        "supportedInterfaces": {
+          "AudioPlayer": {}
+        }
+      },
+      "application": {
+        "applicationId": "amzn1.ask.skill.f35f4e73-39b6-4631-af07-824fecad3215"
+      },
+      "user": {
+        "userId": "amzn1.ask.account.[unique-value-here]"
+      }
+    }
+  }
+}
