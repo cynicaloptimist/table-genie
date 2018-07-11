be887e8d94be36bc1a5b63108e2ceef142659479
Evan Bailey <baileyevang@gmail.com>
2018-07-10 22:08:36 -0700

    Add repeat intent sample

---
+++ sample_repeat.js
@@ -0,0 +1,147 @@
+module.exports = {
+    "session": {
+        "new": false,
+        "sessionId": "amzn1.echo-api.session.[unique-value-here]",
+        "attributes": {
+            "lastPost": {
+                "approved_at_utc": null,
+                "subreddit": "BehindTheTables",
+                "selftext": "Single malt Scotch brand names have been serving me very well when I need a quick location name, here are 50 that I've been using to great effect.\n\n1. Aberlour\n1. Ailsa Bay\n1. Albyn\n1. Ardbeg\n1. Aultmore\n1. Balblair\n1. Ballerchin\n1. Balmenach\n1. The Balvenie\n1. Banff\n1. Bladnoch\n1. Braes\n1. Brora\n1. Cardhu\n1. Caol Ila\n1. Caperdonich\n1. Clynelish\n1. Coleburn\n1. Dailuane\n1. The Dalmore\n1. Deanston\n1. Deveron\n1. Dunglass\n1. Edradour\n1. Elgin\n1. Fettercairn\n1. Finlaggan\n1. Flagler\n1. Garioch\n1. Hazelburn\n1. The Illeach\n1. Inverleven\n1. Kilkerran\n1. Kinclaith\n1. Ledag\n1. Linkwood\n1. Little Mill\n1. Longmorn\n1. Mortlach\n1. Oban\n1. Pittyvaich\n1. Rosebank\n1. Speyburn\n1. Springbank\n1. Strathila\n1. Strathspey\n1. Talisker\n1. Tamdhu\n1. Tullibardine\n1. Wolfburn",
+                "user_reports": [],
+                "saved": false,
+                "mod_reason_title": null,
+                "gilded": 0,
+                "clicked": false,
+                "title": "50 Single Malt Scotch brands that make great Location Names!",
+                "link_flair_richtext": [
+                    {
+                        "e": "text",
+                        "t": "Misc"
+                    }
+                ],
+                "subreddit_name_prefixed": "r/BehindTheTables",
+                "hidden": false,
+                "pwls": null,
+                "link_flair_css_class": "",
+                "downs": 0,
+                "parent_whitelist_status": null,
+                "hide_score": false,
+                "name": "t3_7u8qa2",
+                "quarantine": false,
+                "link_flair_text_color": "dark",
+                "author_flair_background_color": null,
+                "subreddit_type": "public",
+                "ups": 203,
+                "domain": "self.BehindTheTables",
+                "media_embed": {},
+                "author_flair_template_id": null,
+                "is_original_content": false,
+                "secure_media": null,
+                "is_reddit_media_domain": false,
+                "category": null,
+                "secure_media_embed": {},
+                "link_flair_text": "Misc",
+                "can_mod_post": false,
+                "score": 203,
+                "approved_by": null,
+                "thumbnail": "",
+                "edited": false,
+                "author_flair_css_class": null,
+                "author_flair_richtext": [],
+                "content_categories": null,
+                "is_self": true,
+                "mod_note": null,
+                "created": 1517419463,
+                "link_flair_type": "richtext",
+                "wls": null,
+                "post_categories": null,
+                "banned_by": null,
+                "author_flair_type": "text",
+                "contest_mode": false,
+                "selftext_html": "&lt;!-- SC_OFF --&gt;&lt;div class=\"md\"&gt;&lt;p&gt;Single malt Scotch brand names have been serving me very well when I need a quick location name, here are 50 that I&amp;#39;ve been using to great effect.&lt;/p&gt;\n\n&lt;ol&gt;\n&lt;li&gt;Aberlour&lt;/li&gt;\n&lt;li&gt;Ailsa Bay&lt;/li&gt;\n&lt;li&gt;Albyn&lt;/li&gt;\n&lt;li&gt;Ardbeg&lt;/li&gt;\n&lt;li&gt;Aultmore&lt;/li&gt;\n&lt;li&gt;Balblair&lt;/li&gt;\n&lt;li&gt;Ballerchin&lt;/li&gt;\n&lt;li&gt;Balmenach&lt;/li&gt;\n&lt;li&gt;The Balvenie&lt;/li&gt;\n&lt;li&gt;Banff&lt;/li&gt;\n&lt;li&gt;Bladnoch&lt;/li&gt;\n&lt;li&gt;Braes&lt;/li&gt;\n&lt;li&gt;Brora&lt;/li&gt;\n&lt;li&gt;Cardhu&lt;/li&gt;\n&lt;li&gt;Caol Ila&lt;/li&gt;\n&lt;li&gt;Caperdonich&lt;/li&gt;\n&lt;li&gt;Clynelish&lt;/li&gt;\n&lt;li&gt;Coleburn&lt;/li&gt;\n&lt;li&gt;Dailuane&lt;/li&gt;\n&lt;li&gt;The Dalmore&lt;/li&gt;\n&lt;li&gt;Deanston&lt;/li&gt;\n&lt;li&gt;Deveron&lt;/li&gt;\n&lt;li&gt;Dunglass&lt;/li&gt;\n&lt;li&gt;Edradour&lt;/li&gt;\n&lt;li&gt;Elgin&lt;/li&gt;\n&lt;li&gt;Fettercairn&lt;/li&gt;\n&lt;li&gt;Finlaggan&lt;/li&gt;\n&lt;li&gt;Flagler&lt;/li&gt;\n&lt;li&gt;Garioch&lt;/li&gt;\n&lt;li&gt;Hazelburn&lt;/li&gt;\n&lt;li&gt;The Illeach&lt;/li&gt;\n&lt;li&gt;Inverleven&lt;/li&gt;\n&lt;li&gt;Kilkerran&lt;/li&gt;\n&lt;li&gt;Kinclaith&lt;/li&gt;\n&lt;li&gt;Ledag&lt;/li&gt;\n&lt;li&gt;Linkwood&lt;/li&gt;\n&lt;li&gt;Little Mill&lt;/li&gt;\n&lt;li&gt;Longmorn&lt;/li&gt;\n&lt;li&gt;Mortlach&lt;/li&gt;\n&lt;li&gt;Oban&lt;/li&gt;\n&lt;li&gt;Pittyvaich&lt;/li&gt;\n&lt;li&gt;Rosebank&lt;/li&gt;\n&lt;li&gt;Speyburn&lt;/li&gt;\n&lt;li&gt;Springbank&lt;/li&gt;\n&lt;li&gt;Strathila&lt;/li&gt;\n&lt;li&gt;Strathspey&lt;/li&gt;\n&lt;li&gt;Talisker&lt;/li&gt;\n&lt;li&gt;Tamdhu&lt;/li&gt;\n&lt;li&gt;Tullibardine&lt;/li&gt;\n&lt;li&gt;Wolfburn&lt;/li&gt;\n&lt;/ol&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;",
+                "likes": null,
+                "suggested_sort": "new",
+                "banned_at_utc": null,
+                "view_count": null,
+                "archived": false,
+                "no_follow": true,
+                "is_crosspostable": false,
+                "pinned": false,
+                "over_18": false,
+                "media_only": false,
+                "link_flair_template_id": "122fc18e-88d1-11e5-b846-0e68e762d2b5",
+                "can_gild": false,
+                "spoiler": false,
+                "locked": false,
+                "author_flair_text": null,
+                "rte_mode": "markdown",
+                "visited": false,
+                "num_reports": null,
+                "distinguished": null,
+                "subreddit_id": "t5_3aocg",
+                "mod_reason_by": null,
+                "removal_reason": null,
+                "link_flair_background_color": "",
+                "id": "7u8qa2",
+                "report_reasons": null,
+                "author": "NotMyBestUsername",
+                "num_crossposts": 1,
+                "num_comments": 18,
+                "send_replies": true,
+                "mod_reports": [],
+                "author_flair_text_color": null,
+                "permalink": "/r/BehindTheTables/comments/7u8qa2/50_single_malt_scotch_brands_that_make_great/",
+                "whitelist_status": null,
+                "stickied": false,
+                "url": "https://www.reddit.com/r/BehindTheTables/comments/7u8qa2/50_single_malt_scotch_brands_that_make_great/",
+                "subreddit_subscribers": 13971,
+                "created_utc": 1517390663,
+                "media": null,
+                "is_video": false
+            },
+            "lastResult": [
+                {
+                    "rollPrompt": "",
+                    "rollResult": "The Dalmore"
+                }
+            ]
+        },
+        "user": {
+            "userId": "amzn1.ask.account.[unique-value-here]"
+        },
+        "application": {
+            "applicationId": "amzn1.ask.skill.f35f4e73-39b6-4631-af07-824fecad3215"
+        }
+    },
+    "version": "1.0",
+    "request": {
+        "locale": "en-US",
+        "timestamp": "2016-10-27T21:06:28Z",
+        "type": "IntentRequest",
+        "requestId": "amzn1.echo-api.request.[unique-value-here]",
+        "intent": {
+            "slots": {
+
+            },
+            "name": "AMAZON.YesIntent"
+        }
+    },
+    "context": {
+        "AudioPlayer": {
+            "playerActivity": "IDLE"
+        },
+        "System": {
+            "device": {
+                "supportedInterfaces": {
+                    "AudioPlayer": {}
+                }
+            },
+            "application": {
+                "applicationId": "amzn1.ask.skill.f35f4e73-39b6-4631-af07-824fecad3215"
+            },
+            "user": {
+                "userId": "amzn1.ask.account.[unique-value-here]"
+            }
+        }
+    }
+}
